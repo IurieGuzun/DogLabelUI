5364dfcc1a3221609cad6a6f24fb0e28965b85f4
Iurie Guzun <iurie_guzun@yahoo.com>
2022-02-24 13:24:47 -0500

    Initial Commit

---
+++ DogLabelUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DogLabelUI
+//
+//  Created by Iurie Guzun on 2022-02-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+        Image("s")
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DogLabelUI/DogLabelUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  DogLabelUIApp.swift
+//  DogLabelUI
+//
+//  Created by Iurie Guzun on 2022-02-24.
+//
+
+import SwiftUI
+
+@main
+struct DogLabelUIApp: App {
+   
+    var network = Network()  // Added by Iurie
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
